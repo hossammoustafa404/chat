e2a92cf6e65652e02b7abd280b62277381ef1722
hossammoustafa404 <hossammoustafa404@gmail.com>
2025-04-28 14:39:38 +0300

    protect routes && add api bearer auth for swagger docs

--- apps/server/src/modules/auth/auth.controller.ts
+++ apps/server/src/modules/auth/auth.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoginDto, RegisterDto } from './dtos';
+import { LoginDto, LoginResponseDto, RegisterDto } from './dtos';
 import {
+  ApiBadRequestResponse,
   ApiConflictResponse,
   ApiCreatedResponse,
+  ApiOkResponse,
   ApiTags,
+  ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { Serialize } from '../../shared/decorators';
 import { UserDto } from '../user/dtos';
+import { Public } from './decorators';
 
 @ApiTags('Authentication')
 @Controller('auth')
@@ -18,13 +22,23 @@ export class AuthController {
     description: 'The user is successfully registered',
     type: UserDto,
   })
+  @ApiBadRequestResponse({ description: 'Bad request' })
   @ApiConflictResponse({ description: 'Email is taken' })
+  @Public()
   @Serialize(UserDto)
   @Post('register')
   public register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
   }
 
+  @ApiOkResponse({
+    description: 'The user is successfully logged in',
+    type: LoginResponseDto,
+  })
+  @ApiBadRequestResponse({ description: 'Bad request' })
+  @ApiUnauthorizedResponse({ description: 'Bad credentials' })
+  @Public()
+  @HttpCode(HttpStatus.OK)
   @Post('login')
   public login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
